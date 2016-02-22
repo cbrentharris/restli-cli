84b4e5874ffb0d2919420a6c85c870fea84eecbf
Christopher Harris <chharris@linkedin.com>
2016-02-22 15:52:20 -0600

    name of resource needs to be in the package

--- restli/templates/Resource.java
+++ restli/templates/Resource.java
@@ -2,7 +2,7 @@ package {{resource.namespace}}.{{resource.name.lower()}}.impl;
 
 import com.linkedin.restli.server.annotations.RestLiCollection;
 import com.linkedin.restli.server.resources.CollectionResourceTemplate;
-import {{resource.namespace}}.{{resource.name}};
+import {{resource.namespace}}.{{resource.name.lower()}}.{{resource.name}};
 
 @RestLiCollection(name = "{{resource.name.lower()}}s", namespace = "{{resource.namespace}}.{{resource.name.lower()}}")
 public class {{resource.name}}sResource extends CollectionResourceTemplate<Long, {{resource.name}}>
