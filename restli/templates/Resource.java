2546ada75fd879b0809465bd5b986872759d1594
Christopher Harris <chharris@linkedin.com>
2016-02-22 15:12:33 -0600

    need to actually call methods within jinja template, and reference the methods on the resource object

--- restli/templates/Resource.java
+++ restli/templates/Resource.java
@@ -1,14 +1,14 @@
-package {{resource.namespace}}.{{resource.name.lower}}.impl;
+package {{resource.namespace}}.{{resource.name.lower()}}.impl;
 
 import com.linkedin.restli.server.annotations.RestLiCollection;
 import com.linkedin.restli.server.resources.CollectionResourceTemplate;
 import {{resource.namespace}}.{{resource.name}};
 
-@RestLiCollection(name = "{{resource.name.lower}}s", namespace = "{{resource.namespace}}.{{resource.name.lower}}")
+@RestLiCollection(name = "{{resource.name.lower()}}s", namespace = "{{resource.namespace}}.{{resource.name.lower()}}")
 public class {{resource.name}}sResource extends CollectionResourceTemplate<Long, {{resource.name}}>
 {
 
-    {% for method in methods %}
+    {% for method in resource.methods %}
     @Override
     {% if method == 'get' %}
     public {{resource.name}} get(Long key) 
