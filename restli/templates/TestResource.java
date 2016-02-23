05cca01c77fe875f176b538c0b5e33fda52d5501
Christopher Harris <chharris@linkedin.com>
2016-02-23 16:56:05 -0600

    attempting to put gradle into the travis ci build

--- restli/templates/TestResource.java
+++ restli/templates/TestResource.java
@@ -1,24 +1,28 @@
+package {{resource.namespace}}.{{resource.name.lower()}};
 import com.linkedin.r2.transport.http.server.HttpServer;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
 
 public class Test{{resource.name}}sResource
 {
     private HttpServer testServer;
     private static final int PORT = 7777;
-    private static final string HOST = "http://localhost:" + PORT;
+    private static final String HOST = "http://localhost:" + PORT;
 
-    @BeforeTest
+    @Before
     public void init() 
     {
     }
 
     {% for method in resource.methods %}
-    @Test
+//    @Test
     public void test{{method.capitalize()}}()
     {
     }
     {% endfor %}
 
-    @AfterTest
+    @After
     public void cleanup()
     {
     }
